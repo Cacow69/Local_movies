48d755cc326597bce25825762bdb20fce1d418f2
Cacow69 <cmfcursino@gmail.com>
2025-06-08 16:14:55 -0300

    chore(#02): Customização da Página inicial

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 import os
+from datetime import datetime
 
 # Caminho absoluto da pasta "src"
 src_path = os.path.join(os.path.dirname(__file__), "src")
@@ -11,8 +12,19 @@ app = Flask(__name__, template_folder=os.path.join(src_path, "templates"), stati
 # Rotas
 @app.route("/")
 def index():
-    return render_template("index.html")
+    now = datetime.now()
+    day = now.strftime("%a")        
+    time = now.strftime("%H:%M")    
+    datetime_display = f"{day} {time}"
+    return render_template("index.html", datetime_display=datetime_display)
 
+@app.route("/videos")
+def videos():
+    now = datetime.now()
+    day = now.strftime("%a")        
+    time = now.strftime("%H:%M")    
+    datetime_display = f"{day} {time}"
+    return render_template("videos.html", datetime_display=datetime_display)
 
 
 
